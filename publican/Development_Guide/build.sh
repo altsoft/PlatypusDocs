0a32e63fea2f9e205a0de20e977a0b8ceefb40ee
vv <vv@altsoft.biz>
2013-11-28 12:21:12 +0400

    English version added

--- publican/Development_Guide/build.sh
+++ publican/Development_Guide/build.sh
@@ -1,1 +1,2 @@
-publican build --formats pdf,html --langs ru-RU
+publican build --formats pdf,html --langs ru-RU --config publican_ru.cfg
+publican build --formats pdf,html --langs en-US --config publican_en.cfg
