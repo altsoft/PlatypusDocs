c112dbb20f5c9574d74213f698afa35f3a98bc12
vv <vv@altsoft.biz>
2014-02-24 15:01:13 +0400

    The docs fixes.

---
+++ publican/HtmlConvertor/convert.sh
@@ -0,0 +1,3 @@
+java -cp lib/jsoup-1.7.3.jar:build/classes com.eas.htmlconvertor.HtmlConvertor -src ../Development_Guide/tmp/en-US/html -dest ../HtmlConvertor/dist/Development_Guide -layout ../HtmlLayout 
+
+java -cp lib/jsoup-1.7.3.jar:build/classes com.eas.htmlconvertor.HtmlConvertor -src ../Administration_Guide/tmp/en-US/html -dest ../HtmlConvertor/dist/Administration_Guide -layout ../HtmlLayout
