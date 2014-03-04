746a87be08ec47452fd30926da4dd062ef5c003c
vv <vv@altsoft.biz>
2014-03-04 11:06:39 +0400

    Convert HTML script added.

---
+++ publican/convert.sh
@@ -0,0 +1,3 @@
+java -cp ./HtmlConvertor/lib/jsoup-1.7.3.jar:./HtmlConvertor/build/classes com.eas.htmlconvertor.HtmlConvertor -src ./Development_Guide/tmp/en-US/html -dest ./dist/Development_Guide -layout ./HtmlLayout 
+
+java -cp ./HtmlConvertor/lib/jsoup-1.7.3.jar:./HtmlConvertor/build/classes com.eas.htmlconvertor.HtmlConvertor -src ./Administration_Guide/tmp/en-US/html -dest ./dist/Administration_Guide -layout ./HtmlLayout
