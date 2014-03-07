51bd289ff78a3a599baec0048301735bd9e7dfc9
vv <vv@altsoft.biz>
2014-03-07 14:21:44 +0400

    Fixes

--- publican/convert.sh
+++ publican/convert.sh
@@ -1,3 +1,5 @@
+ant -file ./HtmlConvertor/build.xml
+
 java -cp ./HtmlConvertor/lib/jsoup-1.7.3.jar:./HtmlConvertor/build/classes com.eas.htmlconvertor.HtmlConvertor -src ./Development_Guide/tmp/en-US/html -dest ./dist/Development_Guide -layout ./HtmlLayout 
 
 java -cp ./HtmlConvertor/lib/jsoup-1.7.3.jar:./HtmlConvertor/build/classes com.eas.htmlconvertor.HtmlConvertor -src ./Administration_Guide/tmp/en-US/html -dest ./dist/Administration_Guide -layout ./HtmlLayout
