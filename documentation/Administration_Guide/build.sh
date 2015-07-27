762c1cb6c88e9951b8fd48404055dc46000d4b3c
jskonst <jskonst@yandex.ru>
2015-07-27 12:36:50 +0300

    changes in pdf name

--- documentation/Administration_Guide/build.sh
+++ documentation/Administration_Guide/build.sh
@@ -2,5 +2,5 @@ cd en-US
 asciidoc -b html5 -a icons -a toc2 -a toclevels=3 --theme altsoft Administration_Guide.asciidoc
 a2x -fpdf --fop --verbose -dbook -v Administration_Guide.asciidoc
 mv Administration_Guide.html ../../../build/Administration_Guide/index.html 
-mv Administration_Guide.pdf ../../../build/Administration_Guide/Administration_Guide.pdf 
+mv Administration_Guide.pdf ../../../build/Administration_Guide/Administration_Guide-en-US.pdf 
 cp -r images ../../../build/Administration_Guide/
