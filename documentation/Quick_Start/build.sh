23b9d5e60139f0783e3b6f9b7d4a36c829b5426e
jskonst <jskonst@yandex.ru>
2016-06-30 09:59:37 +0300

    Quick start ru fixes

--- documentation/Quick_Start/build.sh
+++ documentation/Quick_Start/build.sh
@@ -4,3 +4,10 @@ a2x -fpdf --fop --verbose -dbook -v Quick_Start.asciidoc
 mv Quick_Start.html ../../../build/Quick_Start/index.html 
 mv Quick_Start.pdf ../../../build/Quick_Start/Quick_Start-en-US.pdf 
 cp -r images ../../../build/Quick_Start/
+
+cd ../ru-RU
+asciidoc -b html5 -a icons -a toc2 -a toclevels=3 --theme altsoft Quick_Start.asciidoc
+a2x -fpdf --fop --verbose -dbook -v Quick_Start.asciidoc
+mv Quick_Start.html ../../../build/Quick_Start_RU/index.html 
+mv Quick_Start.pdf ../../../build/Quick_Start_RU/Quick_Start-ru-RU.pdf 
+cp -r images ../../../build/Quick_Start_RU/
